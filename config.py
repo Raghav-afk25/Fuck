9bafcf702cc37cfdcc3a545696d5c033332a3897
Raghav-afk25 <raghavagarwalq12345@gmail.com>
2025-07-11 19:07:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 API_ID = 123456  # from https://my.telegram.org
 API_HASH = "your_api_hash"
 BOT_TOKEN = "your_bot_token"
-CHANNEL_ID = -100xxxxxxxxxx  # private channel ID where songs will be saved
+CHANNEL_ID = -1002689571440  # private channel ID where songs will be saved
